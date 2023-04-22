f6d07dc22b48a7bdff9828c8e1b82078f2b054bf
EugeneGpil <gpilligrim@gmail.com>
2023-04-22 21:27:29 +0300

    1

--- front/src/app/init.js
+++ front/src/app/init.js
@@ -1,9 +1,11 @@
 import * as THREE from "three";
+import { MapControls } from "three/addons/controls/MapControls.js";
 
 export default (theElement) => {
   const renderer = new THREE.WebGLRenderer({
     canvas: theElement,
-  })
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(
@@ -12,20 +14,31 @@ export default (theElement) => {
     0.1,
     1000
   );
+  window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  });
 
-  const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
-  const cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+  const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+  const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
   scene.add(cube);
 
   camera.position.z = 5;
 
+  const controls = new MapControls(camera, renderer.domElement);
+
   const animate = () => {
-    requestAnimationFrame( animate );
-    renderer.render( scene, camera );
+    requestAnimationFrame(animate);
+
+    // required if controls.enableDamping or controls.autoRotate are set to true
+    // controls.update();
+
+    renderer.render(scene, camera);
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-  }
+  };
   animate();
 };
